8f9c687c4dc1c6b50a8a38afdc0f2218a4c2a529
Mario Viens <marviens@gmail.com>
2016-11-17 18:16:24 -0800

    integrated spotify authorization, WIP

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 require('dotenv').config();
-
 const express = require('express');
 const bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser');
 const PORT = process.env.SERVER_PORT || 8888;
 
+//// Routes
+const spotifyApi = require('./routes/spotify');
 //// Server
 
 const app = express();
 app.set("view engine", "ejs");
 // Tells app to use the public as the static file location
-app.use(express.static('public'));
-
+app.use(express.static('public'))
+   .use(cookieParser());
 //// Middleware
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //// Routes
+
+app.use("/", spotifyApi());
 
 app.get("/", (req, res) => {
   res.render("index");
