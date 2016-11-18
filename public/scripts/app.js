d124f6561cbc8db3db11a1e444742d54a55e8b39
nicadams <nic.adams@hotmail.com>
2016-11-18 05:39:33 +0000

    multiplying of objects, randomized, camera movement

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,21 +1,39 @@
 var camera, scene, renderer;
-var geometry, material, mesh;
+var geometry, material, mesh, controls;
 
 function init() {
 
+
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
-    camera.position.z = 1000;
+    camera.position.z = 1500;
+
+    controls = new THREE.TrackballControls(camera);
+    controls.addEventListener('change', render);
 
     scene = new THREE.Scene();
 
-    geometry = new THREE.BoxGeometry(200, 200, 200);
+    geometry = new THREE.BoxGeometry(100, 100, 100);
+    // geometry2 = new THREE.BoxGeometry(200, 200, 200);
+
     material = new THREE.MeshBasicMaterial({
         color: 0xff0000,
         wireframe: true
     });
 
-    mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
+    // material2 = new THREE.MeshBasicMaterial({
+    //     color: 0xff0000,
+    //     wireframe: true
+    // });
+    for (var i = 0; i < 11; i++) {
+        mesh = new THREE.Mesh(geometry, material);
+        mesh.position.x = (Math.random() - 0.5) * 1000;
+        mesh.position.y = (Math.random() - 0.5) * 1000;
+        mesh.position.z = (Math.random() - 0.5) * 1000;
+
+        scene.add(mesh);
+    }
+
+
 
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -28,14 +46,21 @@ function animate() {
 
     requestAnimationFrame(animate);
 
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.02;
+    // mesh.rotation.x += 0.01;
+    // mesh.rotation.y += 0.02;
 
+    controls.update();
+
+    // renderer.render(scene, camera);
+
+}
+
+function render() {
     renderer.render(scene, camera);
-
 }
 
 document.addEventListener('DOMContentLoaded', function() {
     init();
     animate();
+    render();
 });
