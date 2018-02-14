ab01f5e6ab625234e09393fdf5e8df8150de91d4
Mario Viens <marviens@gmail.com>
2018-02-13 19:41:20 -0800

    reorganizing init scripts

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,8 @@
 $(function(){
   Audio.init();
+  Visualizer.init(visualizer_properties);
 
   $('#my-gui-container').on('click', function(e) {
     Visualizer.sceneRender();
   });
-
-  // Visuals
-  Visualizer.init(visualizer_properties);
 });
